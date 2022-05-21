9f3105e8257116c258621949ab0dbc590753b565
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-05-21 14:00:49 +0300

    Add files via upload

---
+++ Src Code/Cmd_Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMD.Command
+{
+    class Cmd_Command
+    {
+        public void CommandLine(string cmd_command)
+        {
+            ProcessStartInfo ux = new ProcessStartInfo();
+            ux.FileName = "cmd";
+            ux.Arguments = $"/c {cmd_command}";
+            ux.Verb = "runas";
+            ux.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(ux);
+        }
+    }
+}
---
+++ Src Code/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CMD.Command;
+
+namespace MemoryManagementBSODFix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string b = "It's Memory Management Fix... Enjoy to use this :D";
+            MessageBox.Show(b, "Memory Management Fix", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            var key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            var x = key.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management", true);
+            x.SetValue("FeatureSettingsOverride", "3", RegistryValueKind.DWord);
+            x.SetValue("FeatureSettingsOverrideMask", "3", RegistryValueKind.DWord);
+            Thread.Sleep(6000);
+            MessageBox.Show("Fixing Memory Management BSOD is Completed", "Memory Management Fix", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Cmd_Command cmd = new Cmd_Command();
+            cmd.CommandLine("shutdown /r /t 0");
+            Environment.Exit(412);
+        }
+    }
+}
